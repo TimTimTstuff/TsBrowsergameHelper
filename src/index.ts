8bf9ee9d318a4d5512b6c5d51f4b7d9e921333d7
TimTimTstuff <timo.kesselring@tstuff.de>
2020-05-11 16:03:10 +0200

    clear test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import { GameLoopTest } from "../test/gamlooptest";
-import { GameLoop } from "./game/TStuffGame";
-import { LocalStorageSaveHandler } from "./game/gamesave/LocalStorageSaveHandler";
-import { ISaveHandler } from "./game/gamesave/ISaveHandler";
 import { GameSaveTest } from "../test/gamesavetest";
 
 const gameLoopTest = new GameLoopTest()
